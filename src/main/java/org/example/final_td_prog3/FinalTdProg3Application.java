f6c317adf6f124d1824b30c21168e10d2e2ed3d2
Lui100cia <lui100cia@gmail.com>
2026-04-21 14:01:41 +0300

    initial

---
+++ src/main/java/org/example/final_td_prog3/FinalTdProg3Application.java
@@ -0,0 +1,13 @@
+package org.example.final_td_prog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTdProg3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalTdProg3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/final_td_prog3/FinalTdProg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.final_td_prog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalTdProg3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
